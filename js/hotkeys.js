baf0b45c102d44514a444faa539b5da6dd31a6b3
Podvalnyi Mikhail <qwertypomy@gmail.com>
2017-11-25 20:13:26 +0200

    Add form validation and hotkeys

---
+++ js/hotkeys.js
@@ -0,0 +1,18 @@
+document.addEventListener('keydown', function(e) {
+  if (e.ctrlKey && e.altKey) {
+    switch(e.keyCode){
+      case 49:
+        document.getElementById("checkbox-personal-info").checked = true;
+        location.href = location.search + '#personal-info';
+        break;
+      case 50:
+        document.getElementById("checkbox-education").checked = true;
+        location.href = location.search + '#education';
+        break;
+      case 51:
+        document.getElementById("checkbox-language-skills").checked = true;
+        location.href = location.search + '#language-skills';
+        break;
+    }
+  }
+}, false);
